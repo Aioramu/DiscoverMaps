7a32e2f5b8b6cef1311eaa314414ef6a6b9a1f6a
Aioramu <king-gun@mail.ru>
2020-12-10 01:18:40 +0300

    optimized map build

--- maps/src/App.js
+++ maps/src/App.js
@@ -2,43 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 import * as fs from 'fs'
 import { YMaps, Map,GeoObject, Placemark, Polyline, Rectangle, Polygon, Circle, Clusterer, ObjectManager } from 'react-yandex-maps';
-import * as coor from "./coords";
+import * as coor from "./places";
 var coord=new Array(coor)
 //coord.push(coor)
 //console.log(coor[0],typeof coor,typeof coord,coord[0]['default'][0])
 //console.log('sooqa',coord[231]['lon'],coord.length)
-var points=[]
-console.log(points)
-for (var i=0;i<coord[0]['default'].length;i++){
-    points.push([coord[0]['default'][i]['lat'],coord[0]['default'][i]['lon']])
-    console.log(points)
-    //points[i].push(coord[0]['default'][i]['lat'])
-    //points[i].push(coord[0]['default'][i]['lon'])
-  };
-//var points=[[55.751574, 37.573856],[55.758574, 37.573856],[55.765574, 37.573856]]
-console.log(points[0][0])
+console.log(coord[0]['default']['features'][0]['geometry'])
 const App = () => (
 
-  <YMaps>
-  <a href="/usercase/login/">Войти</a>
-    <div>
-    <Map
-  defaultState={{
-    center: [55.751574, 37.573856],
-    zoom: 5,
-  }}>
-  <Clusterer
-    options={{
-      preset: 'islands#invertedVioletClusterIcons',
-      groupByCoordinates: false,
-    }}>
-    {points.map((coordinates, index) => (
-      <Placemark key={index} geometry={coordinates} />
-    ))}
-  </Clusterer>
-  </Map>
-    </div>
-  </YMaps>
-);
+  <div class="map">
+  <div id="entry">
+    <center>
+    <a href="/usercase/login">Войти </a>
+    <a href="/usercase/registration">Зарегестрироваться</a>
+    </center>
+  </div>
+  <YMaps >
 
+  <Map
+    defaultState={{
+      center: [55.751574, 37.573856],
+      zoom: 10,
+    }}
+    width={1500} height={800}>
+    <ObjectManager
+      options={{
+        clusterize: true,
+        gridSize: 32,
+      }}
+      objects={{
+        openBalloonOnClick: true,
+        preset: 'islands#greenDotIcon',
+      }}
+      clusters={{
+        preset: 'islands#redClusterIcons',
+      }}
+      filter={object => object.id % 2 === 0}
+      defaultFeatures={coord[0]['default']['features']}
+      modules={[
+        'objectManager.addon.objectsBalloon',
+        'objectManager.addon.objectsHint',
+      ]}
+    />
+  </Map>
+</YMaps>
+</div>
+);
 export default App;
